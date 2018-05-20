07c9ff3157bc3a093ecf01f4ff9ef2bd6d416e10
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-20 19:36:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear();
+  }
+
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    let hozDist = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+
+    let verDist = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+
+    return hozDist + verDist
+
+  }
+
+  estimatedTime(peak = false){
+    let div = 3
+    if (peak){
+      div = 2
+    }
+    return this.blocksTravelled() / div
+  }
+
+}
